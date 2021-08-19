4862f4c046bf65a993574a9edacf244c2fa0c462
Claudio Iury Lemos Maia - ILE <iury.lemos@iteris.local>
2021-08-19 18:34:33 -0300

    layout created

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,14 @@
 const listMessages = document.querySelector(".chat-messages-list");
 const chatInput = document.querySelector(".chat-input");
+const loading = document.getElementById("loading");
+const buttonSend = document.querySelector(".chat-send");
+const chatMessages = document.querySelector(".chat-messages");
 const messages = [];
 let input = "";
 let context = null;
+loading.style.display = "none";
 
-document.querySelector(".chat-send").addEventListener("click", (evt) => {
+buttonSend.addEventListener("click", (evt) => {
   evt.preventDefault();
   sendMessage();
 });
@@ -13,6 +17,20 @@ chatInput.addEventListener("input", (evt) => {
   evt.preventDefault();
   input = evt.target.outerText;
 });
+
+chatInput.addEventListener("keydown", (evt) => {
+  // evt.preventDefault();
+  if (evt.key === "Enter") {
+    evt.preventDefault();
+    input = evt.target.outerText;
+    sendMessage();
+    // Do more work
+  }
+});
+
+function scrollToBottom() {
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+}
 
 function bubbleUser(text) {
   const li = document.createElement("li");
@@ -48,9 +66,23 @@ function sendInput() {
   };
 }
 
+function setLoad(val) {
+  if (val) {
+    loading.style.display = "flex";
+    buttonSend.setAttribute("disabled", true);
+    chatInput.setAttribute("disabled", true);
+  } else {
+    loading.style.display = "none";
+    buttonSend.removeAttribute("disabled");
+    chatInput.removeAttribute("disabled");
+  }
+}
+
 async function sendMessage() {
   try {
+    chatInput.innerHTML = "";
     const createBuble = bubbleUser(input);
+    setLoad(true);
 
     const resp = await fetch(`${window.location.origin}/api/messages`, {
       method: "POST",
@@ -62,21 +94,29 @@ async function sendMessage() {
     });
     const json = await resp.json();
 
-    // console.log("JSON", json);
+    console.log("JSON", json);
+    setLoad(false);
     context = json.context;
     listMessages.appendChild(createBuble);
 
     if (json.output.text) {
       listMessages.appendChild(bubbleChatbot(json.output.text[0]));
     }
-
-    chatInput.innerHTML = "";
+    scrollToBottom();
   } catch (error) {
-    console.log("error", JSON.parse(JSON.stringify(error)));
+    console.log("error", error);
   }
 }
 
 (() => {
-  listMessages.appendChild(bubbleChatbot("Bom dia"));
-  //   listMessages.appendChild(bubbleUser("Bom dia"));
+  const today = new Date();
+  const curHr = today.getHours();
+
+  if (curHr < 12) {
+    listMessages.appendChild(bubbleChatbot("Bom dia"));
+  } else if (curHr < 18) {
+    listMessages.appendChild(bubbleChatbot("Boa tarde"));
+  } else {
+    listMessages.appendChild(bubbleChatbot("Boa noite"));
+  }
 })();
